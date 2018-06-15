c636b285cb9245c3e25f3da3d3945611f3a18e4b
James Reinhart <jreinhart@orscheln.com>
2018-06-15 11:23:13 -0500

    Initial Commit

---
+++ StringSpliterJava/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String [] args) {
+        AddressBuilder addressOne = new AddressBuilder("101 West Coates St, Moberly MO [Missouri], 63552");
+
+//        String address1 = "101 West Coates St, Moberly MO [Missouri], 63552";
+//        String address2 = "101 West Coates St Moberly, Missouri 63552";
+    }
+}
