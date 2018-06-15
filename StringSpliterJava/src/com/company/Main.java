63623e944e232e9caa71ece9a799e2023912b0f5
James Reinhart <jreinhart@orscheln.com>
2018-06-15 15:50:53 -0500

    Added classes and string manipulation

--- StringSpliterJava/src/com/company/Main.java
+++ StringSpliterJava/src/com/company/Main.java
@@ -3,9 +3,16 @@ package com.company;
 public class Main {
 
     public static void main (String [] args) {
-        AddressBuilder addressOne = new AddressBuilder("101 West Coates St, Moberly MO [Missouri], 63552");
 
-//        String address1 = "101 West Coates St, Moberly MO [Missouri], 63552";
-//        String address2 = "101 West Coates St Moberly, Missouri 63552";
+        String address1 = "101 West Coates St, Moberly MO, 63552";
+        String address2 = "101 West Coates St Moberly, Missouri 63552";
+        String address3 = "1212 Bond St, Moberly MO, 65270";
+
+        AddressBuilder addressBuilder = new AddressBuilder(address3);
+
+        System.out.println("Address: " + addressBuilder.addressObject.getAddress());
+        System.out.println("City: " + addressBuilder.addressObject.getCity());
+        System.out.println("State: " + addressBuilder.addressObject.getState());
+        System.out.println("Zip Code: " + addressBuilder.addressObject.getZipcode());
     }
 }
